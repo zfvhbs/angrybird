36695524953a8b34811acdeee3c442ec72b21d4c
zfvhbs <shettyanmolraj@gmail.com>
2022-05-08 14:06:16 +0530

    testing gitbash

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var platform1
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -18,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+platform1= new Ground( 120,270,250,145);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
